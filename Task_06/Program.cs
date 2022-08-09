c34b1a4ab77ee2a91a49a9a609b4bc6ad1e5c927
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-09 21:29:48 +0700

    add Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 5. Напишите программу, котораяпринимает число (N), а на выходе показывает все целые числа от -N до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = -num-1;
+
+while (i < num)
+{
+    i += 1;
+    Console.Write( i );
+}
